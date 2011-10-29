7c37f1f0d27cf5c2fced8200e02c26aaece74ef2
Erkki Seppälä <flux@modeemi.fi>
2011-10-29 14:25:18 +0300

    Constants: added a file for gloabl constants

---
+++ constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+const int DEPTH_MAP_ORIG_WIDTH = 640;
+const int DEPTH_MAP_ORIG_HEIGHT = 640;
+
+const int DEPTH_MAP_WIDTH = 640;
+const int DEPTH_MAP_HEIGHT = 640;
+
+#endif // CONSTANTS
